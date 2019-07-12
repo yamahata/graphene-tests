bc392d8dfe58050d68d58b845a842d0d264068bb
Isaku Yamahata <isaku.yamahata@gmail.com>
2019-07-12 11:31:25 -0700

    [apps/go] update makefiles and manifests
    Signed-off-by: Isaku Yamahata <isaku.yamahata@gmail.com>

--- go/generate-manifest-template.sh
+++ go/generate-manifest-template.sh
@@ -1,24 +1,35 @@
 #! /bin/zsh
 
-EXEC_FILES=$(ls go/**/*(*) | grep -E -v '\.rc$|\.pl$|^go/misc/|^go/src/cmd/vendor/')
+EXEC_FILES=$(ls go/**/*(*) | grep -E -v '\.rc$|\.pl$|^go/misc/|^go/src/cmd/vendor/|^go/pkg/bootstrap/bin/|testdata|^go/bin/go$')
 # echo ${EXEC_FILES}
 
+# echo ""
+# echo ""
+# echo "# files"
+# for e in ${=EXEC_FILES}
+# do
+#     echo "\t${e} \\"
+# done
 
+
+echo ""
+echo ""
+echo "# trusted files"
 for e in ${=EXEC_FILES}
 do
-    echo "\t${e} \\"
-done
-
-for e in ${=EXEC_FILES}
-do
+    # f=${e:t}
     f=${e//\//_}
     f=${f//\./_}
-    echo "sgx.trusted_files.${f} = file:${e}"
+    echo "sgx.trusted_files.${f} = file:\$(GOROOTDIR)/${e}"
 done
 
+echo ""
+echo ""
+echo "# trusted children"
 for e in ${=EXEC_FILES}
 do
+    # f=${e:t}
     f=${e//\//_}
     f=${f//\./_}
-    echo "sgx.trusted_children.${f} = file:${f}.sig"
+    echo "sgx.trusted_children.${f} = file:\$(GOROOTDIR)/${e:t}.sig"
 done
